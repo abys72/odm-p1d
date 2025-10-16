cc498457ee46149821f561fb11a5b36f08465f21
josea <joseats.hkg@gmail.com>
2025-10-16 15:48:05 +0200

    add mongodb odm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odm-p1d",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         "odmantic",
